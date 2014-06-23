11cb4ce39efd9809b11bde147c06b25a359bc59f
Matt Smith <mcsmith86@mail.com>
2014-06-23 07:40:45 -0700

    intermediate: service route, membershipControl mixin

--- server/models/service.js
+++ server/models/service.js
@@ -3,6 +3,53 @@
 
 var mongoose = require('mongoose');
 
+/*
+http://nagios.sourceforge.net/docs/nagioscore/3/en/objectdefinitions.html#service
+
+define service{
+	*host_name	host_name
+	hostgroup_name	hostgroup_name
+	*service_description	service_description
+	display_name	display_name
+	servicegroups	servicegroup_names
+	is_volatile	[0/1]
+	*check_command	command_name
+	initial_state	[o,w,u,c]
+	*max_check_attempts	#
+	*check_interval	#
+	*retry_interval	#
+	active_checks_enabled	[0/1]
+	passive_checks_enabled	[0/1]
+	*check_period	timeperiod_name
+	obsess_over_service	[0/1]
+	check_freshness	[0/1]
+	freshness_threshold	#
+	event_handler	command_name
+	event_handler_enabled	[0/1]
+	low_flap_threshold	#
+	high_flap_threshold	#
+	flap_detection_enabled	[0/1]
+	flap_detection_options	[o,w,c,u]
+	process_perf_data	[0/1]
+	retain_status_information	[0/1]
+	retain_nonstatus_information	[0/1]
+	*notification_interval	#
+	first_notification_delay	#
+	*notification_period	timeperiod_name
+	notification_options	[w,u,c,r,f,s]
+	notifications_enabled	[0/1]
+	*contacts	contacts
+	*contact_groups	contact_groups
+	stalking_options	[o,w,u,c]
+	notes	note_string
+	notes_url	url
+	action_url	url
+	icon_image	image_file
+	icon_image_alt	alt_string
+}
+*/
+
+
 var serviceSchema = new mongoose.Schema({
 	service_description: {
 		type: String,
@@ -27,7 +74,44 @@ var serviceSchema = new mongoose.Schema({
 	first_notification_delay: Number,
 	notification_intveral: Number,
 	notification_period: String,
-	notification_options: Array,
+	notification_options: {
+
+		warning: {
+			type: Boolean,
+			required: true,
+			default: true
+		},
+
+		recovery: {
+			type: Boolean,
+			required: true,
+			default: true
+		},
+
+		unknown: {
+			type: Boolean,
+			required: true,
+			default: true
+		},
+
+		flapping: {
+			type: Boolean,
+			required: true,
+			default: true
+		},
+
+		critical: {
+			type: Boolean,
+			required: true,
+			default: true
+		},
+
+		scheduled: {
+			type: Boolean,
+			required: true,
+			default: true
+		}
+	},
 	use: Array
 });
 
--- server/routes/service.js
+++ server/routes/service.js
@@ -1,3 +1,4 @@
+
 'use strict';
 /*jslint unparam: true, node: true */
 
@@ -9,12 +10,52 @@
 var async = require('async');
 var mongoose = require('mongoose');
 var Service = mongoose.model("Service");
+var Contact = mongoose.model("Contact");
 var Command = mongoose.model("Command");
 var Host = mongoose.model("Host");
 var HostGroup = mongoose.model("HostGroup");
 
+var isPresent = function(formFieldData){
+	return (formFieldData ? true : false);
+};
+
+var parseRequestBody = function(requestBody){
+	return {
+
+		service_description: requestBody.service_description,
+		check_command: requestBody.check_command,
+
+		// Membership stuff
+		// contacts
+		// contactgroups
+		// hosts
+		// hostgroups
+
+		check_interval: requestBody.check_interval,
+		retry_interval: requestBody.retry_interval,
+		max_check_attempts: requestBody.max_check_attempts,
+		check_period: requestBody.check_period,
+		first_notification_delay: requestBody.first_notification_delay,
+		notifcation_interval: requestBody.notification_interval,
+
+		notification_options: {
+			warning: isPresent(requestBody.notification_options_warning),
+			recovery: isPresent(requestBody.notification_options_recovery),
+			unknown: isPresent(requestBody.notification_options_unknown),
+			flapping: isPresent(requestBody.notification_options_flapping),
+			critical: isPresent(requestBody.notification_options_critical),
+			scheduled: isPresent(requestBody.notification_options_scheduled),
+
+		},
+	};
+};
+
+
 module.exports = function(app){
 
+	// #################################################
+	// #                    INDEX
+	// #################################################
 	app.get('/service', function(req,res){
 		
 		Service.find(function(err, serviceDocs){
@@ -31,51 +72,77 @@ module.exports = function(app){
 		res.redirect('/service');
 	});
 
+
+	// #################################################
+	// #                    ADD
+	// #################################################
 	app.get('/service/add', function(req,res){
 		
 		async.parallel(
 			{
+				contacts: function(callback){
+				 	Contact.find({}, {_id:0, contact_name:1}, callback);
+				},
+
+				// contactgroups: function(callback){
+				//  	ContactGroup.find({}, {_id:0, contactgroup_name:1}, callback);
+				// },
+
 				commands: function(callback){
 					Command.find({}, {_id:0, command_name:1}, callback);
 				},
-				// hosts: function(callback){
-				// 	//Host.find({}, {_id:0, host_name:1}, callback);
-				// },
-				// hostgroups: function(callback){
-				// 	//HostGroup.find({}, {_id:0, hostgroup_name:1}, callback);
+				
+				hosts: function(callback){
+				 	Host.find({}, {_id:0, host_name:1}, callback);
+				},
+				
+				hostgroups: function(callback){
+				 	HostGroup.find({}, {_id:0, hostgroup_name:1}, callback);
+				},
+
+				// servicegroups: function(callback){
+				//  	ServiceGroup.find({}, {_id:0, servicegroup_name:1}, callback);
 				// }
 			},
 
 			function(err,results){
 				if(err){console.log(err);}
 				
-				//console.log(results);
 				res.render('service_form',
 				{
+					contacts: results.contacts,
 					commands: results.commands,
-					// hosts: results.hosts,
-					// hostgroups: results.hostgroups
+					hosts: results.hosts,
+					hostgroups: results.hostgroups
 				});
 			}
 		);
 	});
 	
 	app.post('/service/add', function(req,res){
-		// console.log(req.body);
+		console.log('**********************');
+		console.log(req.body);
+		console.log('**********************');
 		
-		var newService = new Service({
-			service_description: req.body.service_description,
-			check_command: req.body.check_command
-		});
+		var reqBody = parseRequestBody(req.body);
+		console.log(reqBody);
+		res.redirect('/service');
+		
+		// var newService = new Service({
+		// 	service_description: req.body.service_description,
+		// 	check_command: req.body.check_command
+		// });
 
-		newService.save(function(err, service){
-			if(err){ console.log(err); }
-			else {
-				res.redirect('/service');
-			}
-		});
+		// newService.save(function(err, service){
+		// 	if(err){ console.log(err); }
+		// 	res.redirect('/service');
+		// });
 	});
 
+
+	// #################################################
+	// #                    EDIT
+	// #################################################
 	app.get('/service/edit/:service_description', function(req,res){
 
 		Service.findOne({service_description: req.params.service_description}, function(err,serviceDoc){
@@ -102,6 +169,10 @@ module.exports = function(app){
 		});
 	});
 
+
+	// #################################################
+	// #                    DELETE
+	// #################################################
 	app.get('/service/delete/:service_name', function(req,res){
 
 		var question = "Are you sure you want to delete service: " + req.params.service_name + "?";
