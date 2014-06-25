4c0edd84e53c9a0521268f75a5749e0688e3401f
Matt <mcsmith86@gmail.com>
2014-06-25 15:19:53 -0700

    Updated db seeds

--- server/config/mongo-seed.js
+++ server/config/mongo-seed.js
@@ -101,78 +101,69 @@ module.exports = (function(){
 	
 
 	//################### COMMANDS
-	var check_icmp_command = new Command({
+	Command.create({
 		command_name: "check-host-alive-icmp",
 		command_line: "$USER1$/check_icmp -s 216.38.158.8 -H $HOSTADDRESS$ -w 1000.0,60% -c 5000.0,100% -p 5",
 		check_command: true,
 		description: "Checks the ping using source IP 216.38.158.8 with warning threshold 1second/60% loss, critical threshold 5seconds/100% loss, over 5 packets"
 	});
-	check_icmp_command.save();
 
-	var check_ping_command = new Command({
+	Command.create({
 		command_name: "check-host-alive-ping",
 		command_line: "$USER1$/check_ping -s 216.38.158.8 -H $HOSTADDRESS$ -w 1000.0,60% -c 5000.0,100% -p 5",
 		check_command: true,
 		description: "Checks the ping using source IP 216.38.158.8 with warning threshold 1second/60% loss, critical threshold 5seconds/100% loss, over 5 packets"
 	});
-	check_ping_command.save();
 
 
 	//################### HOSTS
-	var host1 = new Host({
+	Host.create({
 		host_name: "host1",
 		alias: "Host One",
 		address: "111.111.111.111",
 		check_command: "check-host-alive-icmp",
 		hostgroups: ["hostgroup1", "hostgroupALL"]
 	});
-	host1.save();
 
-	var host2 = new Host({
+	Host.create({
 		host_name: "host2",
 		alias: "Host One",
 		address: "222.222.222.222",
 		check_command: "check-host-alive-icmp",
 		hostgroups: ["hostgroup2", "hostgroupALL"]
 	});
-	host2.save();
 
-	var host3 = new Host({
+	Host.create({
 		host_name: "host3",
 		alias: "Host One",
 		address: "333.333.333.333",
 		check_command: "check-host-alive-icmp",
 		hostgroups: ["hostgroup3", "hostgroupALL"]
 	});
-	host3.save();
 
 
 	//################### HOSTGROUPS
-	var hg1 = new HostGroup({
+	HostGroup.create({
 		hostgroup_name: "hostgroup1",
 		alias: "Host Group 1",
 		members: ["host1"]
 	});
-	hg1.save();
 
-	var hg2 = new HostGroup({
+	HostGroup.create({
 		hostgroup_name: "hostgroup2",
 		alias: "Host Group 2",
 		members: ["host2"]
 	});
-	hg2.save();
 
-	var hg3 = new HostGroup({
+	HostGroup.create({
 		hostgroup_name: "hostgroup3",
 		alias: "Host Group 3",
 		members: ["host3"]
 	});
-	hg3.save();
 
-	var hgALL = new HostGroup({
+	HostGroup.create({
 		hostgroup_name: "hostgroupALL",
 		alias: "Host Group ALL",
 		members: ["host1", "host2", "host3"]
 	});
-	hgALL.save();
 }());
